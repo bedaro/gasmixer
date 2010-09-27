33a9cadbbf2400c933614c65af3190db1ae3a336
divestoclimb <divestoclimb@3a590330-39e8-11de-b438-597f59cd7555>
2010-09-27 13:33:22 +0000

    2.8.4: synced preferences updates, crash fixes

--- gasmixer/src/divestoclimb/gasmixer/prefs/SyncedPrefsHelper.java
+++ gasmixer/src/divestoclimb/gasmixer/prefs/SyncedPrefsHelper.java
@@ -1,58 +1,32 @@
 package divestoclimb.gasmixer.prefs;
 
+import divestoclimb.android.prefs.AbsSyncedPrefsHelper;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
-import android.database.Cursor;
 import android.net.Uri;
 
-public class SyncedPrefsHelper implements OnSharedPreferenceChangeListener {
+public class SyncedPrefsHelper extends AbsSyncedPrefsHelper {
 
-	private Context mContext;
-
-	public SyncedPrefsHelper(Context context) {
-		mContext = context;
-	}
-
-	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
-			String key) {
-		if(key.equals("units")) {
-			Intent i = new Intent();
-			i.setAction("divestoclimb.scuba.UNIT_CHANGE");
-			i.putExtra("originator", "divestoclimb.gasmixer");
-			i.putExtra("units", Integer.valueOf(sharedPreferences.getString("units", "0")));
-			mContext.sendBroadcast(i);
-		}
-	}
-	
 	private static final Uri[] PEERS = new Uri[] {
 		Uri.parse("content://divestoclimb.scuba.equipment.preferences")
 	};
-	
-	/**
-	 * Queries all my peer preference ContentProviders for one with a value for
-	 * the given key
-	 * @param key
-	 * @return A Cursor that contains a value for the requested preference from a peer
-	 * application, or null if none could be found.
-	 */
-	public Cursor findSetValue(String key) {
-		for(int i = 0; i < PEERS.length; i ++) {
-			Uri p = PEERS[i];
-			if(mContext.getPackageManager().resolveContentProvider(p.getAuthority(), 0) == null) {
-				continue;
-			}
-			Cursor c = mContext.getContentResolver().query(p, null, null, null, null);
-			int index = c.getColumnIndex(key);
-			if(index == -1) {
-				continue;
-			}
-			if(! c.isNull(c.getColumnIndexOrThrow(key))) {
-				return c;
-			}
+
+	public SyncedPrefsHelper(Context context) {
+		super(context, PEERS);
+	}
+
+	@Override
+	protected Intent prepareIntent(SharedPreferences sharedPreferences,
+			String key) {
+		Intent i = null;
+		if(key.equals("units")) {
+			i = new Intent();
+			i.setAction("divestoclimb.scuba.UNIT_CHANGE");
+			i.putExtra("originator", "divestoclimb.gasmixer");
+			i.putExtra("units", Integer.valueOf(sharedPreferences.getString("units", "0")));
 		}
-		return null;
+		return i;
 	}
 
 }
