a11478c75e55a7fba4d36c11ae5f6e08553380d1
divestoclimb <divestoclimb@3a590330-39e8-11de-b438-597f59cd7555>
2010-09-27 13:34:32 +0000

    Synced preferences updates, crash fixes

--- scubatanks/trunk/src/divestoclimb/scuba/equipment/prefs/UpdatePrefs.java
+++ scubatanks/trunk/src/divestoclimb/scuba/equipment/prefs/UpdatePrefs.java
@@ -7,7 +7,6 @@ import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 
 public class UpdatePrefs extends BroadcastReceiver {
 
