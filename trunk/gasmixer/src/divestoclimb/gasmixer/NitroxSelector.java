d48d89b17c8d11fbd4dd73eb486c77ecffcfdb05
divestoclimb <divestoclimb@3a590330-39e8-11de-b438-597f59cd7555>
2011-10-11 13:16:58 +0000

    Split off oxygen input functionality from TrimixSelector into a NitroxSelector superclass

---
+++ trunk/gasmixer/src/divestoclimb/gasmixer/NitroxSelector.java
@@ -0,0 +1,191 @@
+package divestoclimb.gasmixer;
+
+import android.content.Context;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import divestoclimb.android.util.ViewId;
+import divestoclimb.android.widget.NumberSelector;
+import divestoclimb.android.widget.NumberSelector.ValueChangedListener;
+import divestoclimb.lib.scuba.Mix;
+
+public class NitroxSelector extends RelativeLayout implements
+		OnSeekBarChangeListener, ValueChangedListener {
+
+	public static interface MixChangeListener {
+		abstract void onChange(NitroxSelector ns, Mix m);
+	}
+
+	private SeekBar mO2Bar;
+	private NumberSelector mO2Field;
+
+	protected int SELECTOR_LAYOUT = R.layout.nitrox_selector;
+
+	protected MixChangeListener mMixChangeListener;
+	
+	public NitroxSelector(Context context) {
+		super(context);
+		init(context);
+	}
+	
+	public NitroxSelector(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init(context);
+	}
+	
+	protected void init(Context context) {
+		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(SELECTOR_LAYOUT, this);
+
+		mO2Bar = (SeekBar)findViewById(R.id.slider_o2);
+		// We can't keep the slider ID's because they would conflict with other
+		// instances of this class and screw up saving/restoring the SeekBars'
+		// states.
+		// Instead we have to give each SeekBar a random ID, then save and
+		// restore it if the Activity is destroyed and recreated.
+		setViewId(mO2Bar, ViewId.generateUnique(getRootView()));
+
+		mO2Field = (NumberSelector)findViewById(R.id.number_o2);
+		// Because the NumberSelectors save state too, ditto for them
+		setViewId(mO2Field, ViewId.generateUnique(getRootView()));
+
+		mO2Bar.setOnSeekBarChangeListener(this);
+		mO2Field.setValueChangedListener(this);
+	}
+	
+	public NumberSelector getO2Field() {
+		return mO2Field;
+	}
+	
+	protected SeekBar getO2Bar() {
+		return mO2Bar;
+	}
+	
+	public Mix getMix() {
+		Float o2val = mO2Field.getValue();
+		if(o2val == null) {
+			return null;
+		}
+		return new Mix((float)(o2val / 100.0), 0);
+	}
+
+	public void setMix(Mix m) {
+		mO2Field.setValue(m.getO2());
+	}
+
+	public void setOnMixChangeListener(MixChangeListener l) {
+		mMixChangeListener = l;
+	}
+
+	/**
+	 * NumberSelector ValueChangeListener implementation
+	 */
+	@Override
+	public void onChange(NumberSelector ns, Float new_val, boolean from_user) {
+		if(ns == mO2Field) {
+			mO2Bar.setProgress(Math.round(new_val));
+		}
+		if(mMixChangeListener != null) {
+			Mix m = getMix();
+			if(m != null) {
+				mMixChangeListener.onChange(this, m);
+			}
+		}
+	}
+
+	/**
+	 * SeekBarChangeListener implementation
+	 */
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		if(fromUser && seekBar == mO2Bar) {
+			mO2Field.setValue(progress);
+		}
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) { }
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) { }
+	
+	/**
+	 * Set the view ID of the given view, and also update any layout parameters of
+	 * sibling views that referenced it by its ID.
+	 * @param v The view whose ID should be updated
+	 * @param id The new ID to assign the view
+	 */
+	protected void setViewId(View v, int id) {
+		int current_id = v.getId();
+		v.setId(id);
+		for(int i = 0; i < getChildCount(); i++) {
+			RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)getChildAt(i).getLayoutParams();
+			int rules[] = params.getRules();
+			for(int j = 0; j < rules.length; j++) {
+				if(rules[j] == current_id) {
+					params.addRule(j, id);
+				}
+			}
+		}
+	}
+
+	public static class SavedState extends BaseSavedState {
+		int o2SliderId, o2FieldId;
+
+		SavedState(Parcelable superState) {
+			super(superState);
+		}
+
+		@Override
+		public void writeToParcel(Parcel out, int flags) {
+			super.writeToParcel(out, flags);
+
+			out.writeInt(o2SliderId);
+			out.writeInt(o2FieldId);
+		}
+
+		public static final Parcelable.Creator<SavedState> CREATOR
+				= new Parcelable.Creator<SavedState>() {
+			public SavedState createFromParcel(Parcel in) {
+				return new SavedState(in);
+			}
+
+			public SavedState[] newArray(int size) {
+				return new SavedState[size];
+			}
+		};
+
+		protected SavedState(Parcel in) {
+			super(in);
+			o2SliderId = in.readInt();
+			o2FieldId = in.readInt();
+		}
+	}
+
+	@Override
+	public Parcelable onSaveInstanceState() {
+		Parcelable superState = super.onSaveInstanceState();
+
+		SavedState ss = new SavedState(superState);
+		ss.o2SliderId = mO2Bar.getId();
+		ss.o2FieldId = mO2Field.getId();
+
+		return ss;
+	}
+
+	@Override
+	public void onRestoreInstanceState(Parcelable state) {
+		SavedState ss = (SavedState)state;
+		super.onRestoreInstanceState(ss.getSuperState());
+
+		setViewId(mO2Bar, ss.o2SliderId);
+		setViewId(mO2Field, ss.o2FieldId);
+	}
+
+}
--- trunk/gasmixer/src/divestoclimb/gasmixer/TrimixSelector.java
+++ trunk/gasmixer/src/divestoclimb/gasmixer/TrimixSelector.java
@@ -8,89 +8,64 @@ import android.content.res.TypedArray;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.RelativeLayout;
 import android.widget.SeekBar;
 
 /**
  * Custom view for selecting a SCUBA mix
  * @author Ben Roberts (divestoclimb@gmail.com)
  */
-public class TrimixSelector extends RelativeLayout
-		implements SeekBar.OnSeekBarChangeListener, NumberSelector.ValueChangedListener {
+public class TrimixSelector extends NitroxSelector {
 
-	public static interface MixChangeListener {
-		abstract void onChange(TrimixSelector ts, Mix m);
-	}
-
-	private LayoutInflater mInflater;
-	private SeekBar mO2Bar, mHeBar;
-	private NumberSelector mO2Field, mHeField;
-
-	protected final int SELECTOR_LAYOUT = R.layout.trimix_selector;
-
-	private MixChangeListener mMixChangeListener;
+	private SeekBar mHeBar;
+	private NumberSelector mHeField;
 
 	public TrimixSelector(Context context) {
 		super(context);
-		initTrimixSelector(context);
 	}
 	public TrimixSelector(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		initTrimixSelector(context);
 		
 		TypedArray a = context.obtainStyledAttributes(attrs,
 				R.styleable.TrimixSelector);
 		mHeField.setIncrement(a.getInt(R.styleable.TrimixSelector_heIncrement, 5));
-		mO2Field.setLimits(Float.valueOf(a.getInt(R.styleable.TrimixSelector_minO2, 5)), 100f);
+		getO2Field().setLimits(Float.valueOf(a.getInt(R.styleable.TrimixSelector_minO2, 5)), 100f);
 	}
 
+	@Override
 	public Mix getMix() {
-		Float o2val = mO2Field.getValue(), heval = mHeField.getValue();
+		Float o2val = getO2Field().getValue(), heval = mHeField.getValue();
 		if(o2val == null || heval == null) {
 			return null;
 		}
 		return new Mix((float)(o2val / 100.0), (float)(heval / 100.0));
 	}
 
+	@Override
 	public void setMix(Mix m) {
-		mO2Field.setValue(m.getO2());
+		super.setMix(m);
 		mHeField.setValue(m.getHe());
 	}
 
-	public void setOnMixChangeListener(MixChangeListener l) {
-		mMixChangeListener = l;
-	}
+	@Override
+	protected void init(Context context) {
+		SELECTOR_LAYOUT = R.layout.trimix_selector;
+		super.init(context);
 
-	private void initTrimixSelector(Context context) {
-		mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		mInflater.inflate(SELECTOR_LAYOUT, this);
-
-		mO2Bar = (SeekBar)findViewById(R.id.slider_o2);
 		mHeBar = (SeekBar)findViewById(R.id.slider_he);
 		// We can't keep the slider ID's because they would conflict with other
 		// instances of this class and screw up saving/restoring the SeekBars'
 		// states.
 		// Instead we have to give each SeekBar a random ID, then save and
 		// restore it if the Activity is destroyed and recreated.
-		setViewId(mO2Bar, ViewId.generateUnique(getRootView()));
 		setViewId(mHeBar, ViewId.generateUnique(getRootView()));
 
-		mO2Field = (NumberSelector)findViewById(R.id.number_o2);
+
 		mHeField = (NumberSelector)findViewById(R.id.number_he);
 		// Because the NumberSelectors save state too, ditto for them
-		setViewId(mO2Field, ViewId.generateUnique(getRootView()));
 		setViewId(mHeField, ViewId.generateUnique(getRootView()));
 
-		mO2Bar.setOnSeekBarChangeListener(this);
 		mHeBar.setOnSeekBarChangeListener(this);
-		mO2Field.setValueChangedListener(this);
 		mHeField.setValueChangedListener(this);
-	}
-	
-	public NumberSelector getO2Field() {
-		return mO2Field;
 	}
 	
 	public NumberSelector getHeField() {
@@ -100,51 +75,37 @@ public class TrimixSelector extends RelativeLayout
 	/**
 	 * SeekBarChangeListener implementation
 	 */
-	public void onProgressChanged(SeekBar slider, int newPosition, boolean from_user) {
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+		super.onProgressChanged(seekBar, progress, fromUser);
 		NumberSelector field;
-		if(slider == mO2Bar) {
-			field = mO2Field;
-		} else {
+		if(seekBar == mHeBar) {
 			field = mHeField;
+			field.setValue(progress);
+		} else {
+			field = getO2Field();
 		}
-		if(from_user) {
-			field.setValue(newPosition);
+		if(fromUser) {
 			handlePercentUpdate(field);
 		}
 	}
 
-	public void onStartTrackingTouch(SeekBar arg0) { }
-	public void onStopTrackingTouch(SeekBar arg0) { }
-
 	/**
 	 * NumberSelector ValueChangeListener implementation
 	 */
+	@Override
 	public void onChange(NumberSelector ns, Float new_val, boolean from_user) {
-		SeekBar sb;
-		if(ns == mO2Field) {
-			sb = mO2Bar;
-		} else {
-			sb = mHeBar;
+		if(ns == mHeField) {
+			mHeBar.setProgress(Math.round(new_val));
 		}
-		sb.setProgress(Math.round(new_val));
 		if(from_user) {
 			handlePercentUpdate(ns);
 		}
-		if(mMixChangeListener != null) {
-			Mix m = getMix();
-			if(m != null) {
-				mMixChangeListener.onChange(this, m);
-			}
-		}
+		super.onChange(ns, new_val, from_user);
 	}
 
 	private void handlePercentUpdate(NumberSelector field) {
-		NumberSelector other_field;
-		if(field == mO2Field) {
-			other_field = mHeField;
-		} else {
-			other_field = mO2Field;
-		}
+		NumberSelector other_field = field == mHeField? getO2Field(): mHeField;
 		float field_value = field.getValue();
 		if(field_value + other_field.getValue() > 100) {
 			other_field.setValue(100 - field_value);
@@ -157,28 +118,8 @@ public class TrimixSelector extends RelativeLayout
 		}
 	}
 
-	/**
-	 * Set the view ID of the given view, and also update any layout parameters of
-	 * sibling views that referenced it by its ID.
-	 * @param v The view whose ID should be updated
-	 * @param id The new ID to assign the view
-	 */
-	protected void setViewId(View v, int id) {
-		int current_id = v.getId();
-		v.setId(id);
-		for(int i = 0; i < getChildCount(); i++) {
-			RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams)getChildAt(i).getLayoutParams();
-			int rules[] = params.getRules();
-			for(int j = 0; j < rules.length; j++) {
-				if(rules[j] == current_id) {
-					params.addRule(j, id);
-				}
-			}
-		}
-	}
-
 	public static class SavedState extends BaseSavedState {
-		int o2SliderId, heSliderId, o2FieldId, heFieldId;
+		int heSliderId, heFieldId;
 
 		SavedState(Parcelable superState) {
 			super(superState);
@@ -188,9 +129,7 @@ public class TrimixSelector extends RelativeLayout
 		public void writeToParcel(Parcel out, int flags) {
 			super.writeToParcel(out, flags);
 
-			out.writeInt(o2SliderId);
 			out.writeInt(heSliderId);
-			out.writeInt(o2FieldId);
 			out.writeInt(heFieldId);
 		}
 
@@ -207,9 +146,7 @@ public class TrimixSelector extends RelativeLayout
 
 		private SavedState(Parcel in) {
 			super(in);
-			o2SliderId = in.readInt();
 			heSliderId = in.readInt();
-			o2FieldId = in.readInt();
 			heFieldId = in.readInt();
 		}
 	}
@@ -219,9 +156,7 @@ public class TrimixSelector extends RelativeLayout
 		Parcelable superState = super.onSaveInstanceState();
 
 		SavedState ss = new SavedState(superState);
-		ss.o2SliderId = mO2Bar.getId();
 		ss.heSliderId = mHeBar.getId();
-		ss.o2FieldId = mO2Field.getId();
 		ss.heFieldId = mHeField.getId();
 
 		return ss;
@@ -232,9 +167,7 @@ public class TrimixSelector extends RelativeLayout
 		SavedState ss = (SavedState)state;
 		super.onRestoreInstanceState(ss.getSuperState());
 
-		setViewId(mO2Bar, ss.o2SliderId);
 		setViewId(mHeBar, ss.heSliderId);
-		setViewId(mO2Field, ss.o2FieldId);
 		setViewId(mHeField, ss.heFieldId);
 	}
 
